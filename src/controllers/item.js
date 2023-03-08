f7305fc51db2740298b1c849ccf48700fd0d8bce
mixx-code <rfebriansyah385@gmail.com>
2023-03-08 22:22:54 +0700

    membuat item masuk

--- src/controllers/item.js
+++ src/controllers/item.js
@@ -53,8 +53,8 @@ exports.getAllItem = (req, res, next) => {
 };
 
 exports.getItemById = (req, res, next) => {
-  const postId = req.params.itemId;
-  ItemPost.findById(postId)
+  const itemId = req.params.itemId;
+  ItemPost.findById(itemId)
     .then((result) => {
       if (!result) {
         const error = new Error("Blog Post tidak ditemukan");
@@ -63,6 +63,63 @@ exports.getItemById = (req, res, next) => {
       }
       res.status(200).json({
         message: "Data Blog Post berhasil Dipanggil",
+        data: result,
+      });
+    })
+    .catch((err) => next(err));
+};
+
+exports.updateItem = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const err = new Error("Input value tidak sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
+  const nama_item = req.body.nama_item;
+  const total_stok = req.body.total_stok;
+
+  const itemId = req.params.itemId;
+
+  ItemPost.findById(itemId)
+    .then((result) => {
+      if (!result) {
+        const err = new Error("Blog tidak ditemukan");
+        err.errorStatus = 404;
+        throw err;
+      }
+      result.nama_item = nama_item;
+      result.total_stok = total_stok;
+
+      return result.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Update Success",
+        data: result,
+      });
+    })
+    .catch((err) => next(err));
+};
+
+exports.deleteItem = (req, res, next) => {
+  const itemId = req.params.itemId;
+
+  ItemPost.findById(itemId)
+    .then((result) => {
+      if (!result) {
+        const err = new Error("Blog tidak ditemukan");
+        err.errorStatus = 404;
+        throw err;
+      }
+      //setelah image berhasil diremove sekarang menghapus postingan nya
+      return ItemPost.findByIdAndDelete(itemId);
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Hapus Blog Post Berhasil",
         data: result,
       });
     })
---
+++ src/models/itemMasuk.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+mongoose.set("strictQuery", false);
+const ItemMasuk = new Schema(
+  {
+    nama_item: {
+      type: String,
+      required: true,
+    },
+    jumlah_item_masuk: {
+      type: Number,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("ItemMasuk", ItemMasuk);
